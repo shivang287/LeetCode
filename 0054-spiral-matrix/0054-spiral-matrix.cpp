a84f3fa381ed109c84414f97e542d3f8473bf41d
Shivang Mishra <mishrashivang212@gmail.com>
2026-08-23 14:46:13 +0530

    Time: 3 ms (0.73%), Space: 9.3 MB (57.58%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& mat) {
-        int srow = 0;
-        int scol = 0;
-        int erow = mat.size() - 1;
-        int ecol = mat[0].size() - 1;
-
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> ans;
-
-        while (srow <= erow && scol <= ecol) {
-
-            // Top row
-            for (int i = scol; i <= ecol; i++)
-                ans.push_back(mat[srow][i]);
-
-            // Right column
-            for (int i = srow + 1; i <= erow; i++)
-                ans.push_back(mat[i][ecol]);
-
-            // Bottom row
-            if (srow < erow) {
-                for (int i = ecol - 1; i >= scol; i--)
-                    ans.push_back(mat[erow][i]);
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int srow =0,scol=0,erow=m-1,ecol=n-1;
+        while(srow<=erow && scol <= ecol){
+            for(int i=scol; i<=ecol; i++){
+                ans.push_back(matrix[srow][i]);
             }
-
-            // Left column
-            if (scol < ecol) {
-                for (int i = erow - 1; i > srow; i--)
-                    ans.push_back(mat[i][scol]);
+            for(int i=srow+1; i<=erow; i++){
+                ans.push_back(matrix[i][ecol]);
             }
-
+            for(int i=ecol-1; i>=scol; i--){
+                if(srow == erow){
+                    break;
+                }
+                ans.push_back(matrix[erow][i]);
+            }
+            for(int i=erow-1; i>=srow+1; i--){
+                if(scol == ecol){
+                    break;
+                }
+                ans.push_back(matrix[i][scol]);
+            }
             srow++;
-            erow--;
             scol++;
+            erow--;
             ecol--;
         }
-
         return ans;
     }
 };
